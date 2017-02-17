bf9e5c6b8b938f1219c4a808a355887c4b5e5818
Yi JIANG <jiang.yi@siphty.com>
2017-02-17 12:06:13 +1100

    Initial Commit

---
+++ Stereoscopic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stereoscopic
+//
+//  Created by Yi JIANG on 17/2/17.
+//  Copyright © 2017 RobertYiJiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
